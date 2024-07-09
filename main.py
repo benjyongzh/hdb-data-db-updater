42cd8bdaf19bf55168936243f11e2b33bf518a3d
benjyongzh <benj_yongzh@hotmail.com>
2024-07-09 23:10:59 +0800

    Add query for testing on csv file. with inline sql vscode extension

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ conn = psycopg2.connect(host=config["HOST"],dbname = config["DBNAME"], user=conf
 cur = conn.cursor()
 
 #do stuff
-cur.execute("""SELECT * FROM practice;""")
+cur.execute("""--sql SELECT * FROM practice;""")
 for row in cur.fetchall():
     print(row)
+
+cur.execute("""--sql CREATE TEMP TABLE tmp_table AS SELECT * FROM practice WITH NO DATA;""")
+
+cur.execute("""--sql COPY tmp_table FROM '<file-here>.csv' DELIMITER ',' CSV;""")
+            
+cur.execute("""--sql INSERT INTO practice SELECT * FROM tmp_table ON CONFLICT DO NOTHING;""")
+
+cur.execute("""--sql DROP TABLE tmp_table;""")
 
 conn.commit()
 
