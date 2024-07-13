f81c3e013ae78f67cd64d2c986be3a10a2e3d63d
benjyongzh <benj_yongzh@hotmail.com>
2024-07-14 00:45:24 +0800

    Add next steps

--- datacollector/urls.py
+++ datacollector/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('resaleprices/', views.get_resale_prices),
+    path('resaleprices/', views.get_resale_prices),# find out how to do params for filtering
     path('buildingpolygons/', views.get_building_polygons),
 ]
