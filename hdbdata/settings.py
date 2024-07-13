aa52e60b173bfc6f61e9696b11adfe4fb6367e29
benjyongzh <benj_yongzh@hotmail.com>
2024-07-13 17:10:54 +0800

    Add django-debug-toolbar

--- hdbdata/settings.py
+++ hdbdata/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground'
+    'playground',
+    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hdbdata.urls'
+
+INTERNAL_IPS = [
+    '127.0.0.1'
+]
 
 TEMPLATES = [
     {
