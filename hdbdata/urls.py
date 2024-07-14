cc81268138cc8bbf1f8d89385dc599cb15f801b7
benjyongzh <benj_yongzh@hotmail.com>
2024-07-14 22:01:22 +0800

    Add dataupdater URL

--- hdbdata/urls.py
+++ hdbdata/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
     path('data/', include('datacollector.urls')),
+    path('update/', include('dataupdater.urls')),
     
 ]
