2db919561bbdd38aaeb164148e173ac82030c1c5
benjyongzh <benj_yongzh@hotmail.com>
2024-07-18 21:41:59 +0800

    Create function for getting latest file in a directory

--- common/util/get_latest_file_in_folder.py
+++ common/util/get_latest_file_in_folder.py
@@ -1,4 +1,7 @@
-
+import os
+import glob
 
 def get_latest_file_in_folder(folderpath):
-    pass
+    list_of_files = glob.glob(folderpath + '*.csv') # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file
