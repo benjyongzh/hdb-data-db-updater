5d7111810a7ed7efb2ff336554678ad38cea2e9c
benjyongzh <benj_yongzh@hotmail.com>
2024-07-18 22:09:32 +0800

    Update query

--- datacollector/query-latest-data.sql
+++ datacollector/query-latest-data.sql
@@ -1,11 +1,4 @@
-SELECT DISTINCT ON (full_unit_name) * FROM
-	(SELECT id, month, town, (
-		SELECT street_name
-		|| ' ' || block
-		|| ' ' || flat_type
-		|| ' ' || flat_model
-		|| ' ' || storey_range
-		|| ' ' || floor_area_sqm AS full_unit_name),
-	resale_price FROM datacollector_resaletransaction
-	ORDER BY full_unit_name, month, id DESC) 
+SELECT DISTINCT ON (street_name, block, flat_type, flat_model, storey_range, floor_area_sqm) * FROM
+	(SELECT * FROM datacollector_resaletransaction
+	ORDER BY street_name, block, flat_type, flat_model, storey_range, floor_area_sqm, month, id DESC)
 orderedbyunitname;
