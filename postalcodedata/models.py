7e658d4d33b8ecf0d7d14de424fffa63459f2301
benjyongzh <benj_yongzh@hotmail.com>
2024-07-19 20:03:34 +0800

    Add model for table with address and postal codes

---
+++ postalcodedata/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class PostalCodeAddress(models.Model):
+    block = models.CharField(max_length=4)
+    street_name = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=6)
+
+    def __str__(self):
+        return f"{self.block} {self.month} {self.postal_code}"
+    
+    def get_keys():
+        return "block, street_name, postal_code"
+    
+    def address(self):
+        return f"{self.block} {self.street_name}"
+
