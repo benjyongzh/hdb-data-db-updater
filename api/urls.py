3c01e5757931e46459465df59caf15c215080ecc
benjyongzh <benj_yongzh@hotmail.com>
2024-07-21 13:45:50 +0800

    Fix: merge urlpatterns

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('resale-prices/', views.get_resale_prices),
-]
-
-urlpatterns = [
+     path('resale-transactions/', views.get_resale_prices),
      path('postal-codes/', views.get_postal_codes),
 ]
