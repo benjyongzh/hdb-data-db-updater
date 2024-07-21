96c062eefe2b216ad3efdecfe6d1a2465769acd4
benjyongzh <benj_yongzh@hotmail.com>
2024-07-21 10:22:00 +0800

    Refactor: Updating of new transactions to be clearer

--- resaletransactions/urls.py
+++ resaletransactions/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('resaleprices/', views.update_resale_prices),# find out how to do params for filtering
+    path('update-new-transactions/', views.update_new_transactions),# find out how to do params for filtering
     path('buildingpolygons/', views.update_building_polygons),
 ]
--- resaletransactions/views.py
+++ resaletransactions/views.py
@@ -1,21 +1,27 @@
-from django.http import HttpResponse
-import psycopg2
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from dotenv import dotenv_values
-from django.shortcuts import redirect
-from api import views as api_views
 from common.util.import_csv_to_db import import_from_csv_to_db
 
 config = dotenv_values(".env")
 
 # Create your views here.
-def update_resale_prices(request):
+@api_view(['GET'])
+def update_new_transactions(request):
     if config["ENV"] == "DEV":
-        import_from_csv_to_db("datacollector_resaletransaction", config["RESALE_PRICE_CSV_FOLDER_PATH_DEV"])
+        import_from_csv_to_db("resaletransactions_resaletransaction", config["RESALE_PRICE_CSV_FOLDER_PATH_DEV"])
     elif config["ENV"] == "PROD":
-        import_from_csv_to_db("datacollector_resaletransaction", config["RESALE_PRICE_CSV_FOLDER_PATH_PROD"])
-    return redirect(api_views.get_resale_prices)
+        import_from_csv_to_db("resaletransactions_resaletransaction", config["RESALE_PRICE_CSV_FOLDER_PATH_PROD"])
+    else:
+        # respond with not ok. wrong config
+        data = {"error-message": "Invalid configurations"}
+        return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    # respond with ok
+    data = {"redirect_url": "/api/resale-transactions/"}
+    return Response(data, status=status.HTTP_200_OK)
 
 
 #! figure out how to merge new data with building polygons data to create a final table that merges resale prices and building polygons
 def update_building_polygons(request):
-    return HttpResponse("update building polygons here")
+    return Response("update building polygons here", status=status.HTTP_200_OK)
