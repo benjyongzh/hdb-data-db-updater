95f3254d37aabf7b1f754ce6e7f2fc5714d91e6b
benjyongzh <benj_yongzh@hotmail.com>
2024-07-23 21:17:10 +0800

    Add steps to take

--- postalcodes/util/parse_geojson.py
+++ postalcodes/util/parse_geojson.py
@@ -25,11 +25,33 @@ def get_geometry_from_feature(feature):
     return feature['geometry']
 
 filepath = "/home/benjyongzh/hdb-info/building-polygons/HDBExistingBuilding-sample-unformatted.geojson"
-with open(filepath) as f:
-    gj = geojson.load(f)
-    print(get_block_number_from_feature(gj['features'][0]))
+# with open(filepath) as f:
+#     gj = geojson.load(f)
+#     print(get_block_number_from_feature(gj['features'][0]))
 
-# def import_new_geojson_features_into_postgis(file):
+def import_new_geojson_features_into_polygon_table(filepath):
+    pass
     # iterate through every feature
-    # for each feature, get postalcode
-    # if postalcode doesnt exist in db, get geometry and create new row with postalcode + geometry. use 
+    with open(filepath) as f:
+        gj = geojson.load(f)
+
+        # for each feature, get postalcode
+        features = gj['features']
+        for feature in features:
+            postal_code = get_postal_code_from_feature(gj)
+
+            # if postalcode doesnt exist in db:
+            
+            # get geometry object,
+            # remove z axis from geometry, use first index of coordinates only
+            # create new row with postalcode + geometry,
+            # use SELECT ST_GeomFromGeoJSON(<geometry>),ST_AsText,ST_AsGeoJSON
+
+def remove_z_from_coordinates(coordinates):
+    arr = coordinates[0]
+    for point in arr:
+        point = [point[0], point[1]]
+    return arr
+
+def add_new_row_into_polygon_table(feature):
+    pass
