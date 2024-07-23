fb901ea10ad2361336436dfb408df0b3b0dd9780
benjyongzh <benj_yongzh@hotmail.com>
2024-07-23 20:52:21 +0800

    Add function for getting block number from a geojson feature

--- postalcodes/util/parse_geojson.py
+++ postalcodes/util/parse_geojson.py
@@ -4,14 +4,22 @@ from bs4 import BeautifulSoup
 def get_postal_code_from_feature(feature) -> str:
     description = feature['properties']['Description']
     postal_code:str = get_postal_code_from_description(description)
-    # geometry = feature['geometry']
     return postal_code
-
 
 def get_postal_code_from_description(description) -> str:
     table_data = [[cell.text for cell in row("td")]
         for row in BeautifulSoup(description, features="html.parser")("tr")]
     return table_data[4][0]
+
+def get_block_number_from_feature(feature) -> str:
+    description = feature['properties']['Description']
+    block:str = get_block_number_from_description(description)
+    return block
+
+def get_block_number_from_description(description) -> str:
+    table_data = [[cell.text for cell in row("td")]
+        for row in BeautifulSoup(description, features="html.parser")("tr")]
+    return table_data[1][0]
 
 def get_geometry_from_feature(feature):
     return feature['geometry']
@@ -19,4 +27,9 @@ def get_geometry_from_feature(feature):
 filepath = "/home/benjyongzh/hdb-info/building-polygons/HDBExistingBuilding-sample-unformatted.geojson"
 with open(filepath) as f:
     gj = geojson.load(f)
-    print(get_postal_code_from_feature(gj['features'][0]))
+    print(get_block_number_from_feature(gj['features'][0]))
+
+# def import_new_geojson_features_into_postgis(file):
+    # iterate through every feature
+    # for each feature, get postalcode
+    # if postalcode doesnt exist in db, get geometry and create new row with postalcode + geometry. use 
