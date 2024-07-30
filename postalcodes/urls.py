0b1ae87e4e612433282846a67e7f15a1234f5ae8
benjyongzh <benj_yongzh@hotmail.com>
2024-07-30 22:30:18 +0800

    Add URL for updating building polygons

--- postalcodes/urls.py
+++ postalcodes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('update/', views.update_postal_code_data),
+     path('update/postal_codes', views.update_postal_code_data),
+     path('update/polygons', views.update_new_building_polygons),
 ]
