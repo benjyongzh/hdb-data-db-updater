038fe9e3f116bbb9495b26382ffc5e62177567a3
benjyongzh <benj_yongzh@hotmail.com>
2024-08-03 16:50:41 +0800

    Clear sql files

--- datacollector/query-all-addresses.sql
+++
@@ -1,2 +0,0 @@
-SELECT DISTINCT ON (street_name, block) block,street_name FROM datacollector_resaletransaction
-	ORDER BY street_name, block;
--- datacollector/query-latest-data.sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT ON (street_name, block, flat_type, flat_model, storey_range, floor_area_sqm) * FROM
-	(SELECT * FROM datacollector_resaletransaction
-	ORDER BY street_name, block, flat_type, flat_model, storey_range, floor_area_sqm, month, id DESC)
-orderedbyunitname;
