f4c1a4b21ea70ff95da88787e18290d1c16667f1
benjyongzh <benj_yongzh@hotmail.com>
2024-08-10 01:40:59 +0800

    Add page for updating postalcodes, with methods in admin class

--- postalcodes/admin.py
+++ postalcodes/admin.py
@@ -10,6 +10,27 @@ from postalcodes.util.parse_geojson import import_new_geojson_features_into_tabl
 class PostalCodeAddressAdmin(admin.ModelAdmin):
     list_display = ['block', 'street_name', 'postal_code']
 
+    def get_urls(self):
+        urls = super().get_urls()
+        new_urls = [path('update-mapping/', self.update_postalcode_address_table),]
+        return new_urls + urls
+
+    def update_postalcode_address_table(self, request):
+        if request.method == "POST":
+            return process_file_upload(
+                request,
+                request_file_key='input_file',
+                file_ext='.geojson',
+                success_try_func=upload_geojson_impl,
+                success_redirect_url="/api/building-polygons/"
+            )
+        else:
+            form_context = {
+                'form_title': "Refresh and update the mapping table of postal codes and addresses",
+                'table_name': "postalcodes_postalcodeaddress"
+            }
+            return render(request, "admin/update_mapping.html", context=form_context)
+
 @admin.register(BuildingGeometryPolygon)
 class BuildingGeometryPolygonAdmin(admin.ModelAdmin):
     list_display = ['block', 'postal_code', 'building_polygon']
