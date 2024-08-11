9c9360815a3be5fa417ac1e74df99c72ad9d7ff0
benjyongzh <benj_yongzh@hotmail.com>
2024-08-11 22:38:44 +0800

    Add SQL file for easy reference

---
+++ resaletransactions/reset-data.sql
@@ -0,0 +1,13 @@
+-- DELETE FROM resaletransactions_resaletransaction WHERE id > 0;
+-- ALTER SEQUENCE resaletransactions_resaletransaction_id_seq RESTART WITH 1;
+-- SELECT * FROM resaletransactions_resaletransaction ORDER BY id
+
+
+-- DELETE FROM postalcodes_buildinggeometrypolygon WHERE id > 0;
+-- ALTER SEQUENCE postalcodes_buildinggeometrypolygon_id_seq RESTART WITH 1;
+-- SELECT * FROM postalcodes_buildinggeometrypolygon ORDER BY id;
+
+
+-- DELETE FROM postalcodes_postalcodeaddress WHERE id > 0;
+-- ALTER SEQUENCE postalcodes_postalcodeaddress_id_seq RESTART WITH 1;
+SELECT * FROM postalcodes_postalcodeaddress
