0939f5f4918e81e89fe3e509549a5404233b2212
benjyongzh <benj_yongzh@hotmail.com>
2024-08-14 22:38:14 +0800

    Fix: try-except for db entries

--- timestamps/admin.py
+++ timestamps/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from timestamps.models import TablesLastUpdated
 from api.serializers import TablesLastUpdatedSerializer
 from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
 
 # Register your models here.
 @admin.register(TablesLastUpdated)
@@ -9,15 +10,23 @@ class TablesLastUpdatedAdmin(admin.ModelAdmin):
     list_display = ['table', 'last_updated']
 
 def update_table_last_updated(table_name:str):
-    last_updated = TablesLastUpdated.objects.update_or_create(table=table_name)
-    serializer = TablesLastUpdatedSerializer(last_updated)
-    print(serializer.data)
-    date = datetime(serializer.data)
-    return date.strftime('%a %d %b %Y, %I:%M%p')
+    try:
+        last_updated = TablesLastUpdated.objects.update(table=table_name)
+    except ObjectDoesNotExist as e:
+        last_updated = TablesLastUpdated.objects.create(table=table_name)
+    finally:
+        serializer = TablesLastUpdatedSerializer(last_updated)
+        raw_date = serializer.data['last_updated'].split(".")[0]
+        date = datetime.strptime(raw_date,'%Y-%m-%dT%H:%M:%S')
+        return date.strftime('%a %d %b %Y, %I:%M%p')
     
 def get_table_last_updated(table_name:str):
-    last_updated = TablesLastUpdated.objects.get(table=table_name)
-    serializer = TablesLastUpdatedSerializer(last_updated)
-    raw_date = serializer.data['last_updated'].split(".")[0]
-    date = datetime.strptime(raw_date,'%Y-%m-%dT%H:%M:%S')
-    return date.strftime('%a %d %b %Y, %I:%M%p')
+    try:
+        last_updated = TablesLastUpdated.objects.get(table=table_name)
+    except ObjectDoesNotExist as e:
+        last_updated = TablesLastUpdated.objects.create(table=table_name)
+    finally:
+        serializer = TablesLastUpdatedSerializer(last_updated)
+        raw_date = serializer.data['last_updated'].split(".")[0]
+        date = datetime.strptime(raw_date,'%Y-%m-%dT%H:%M:%S')
+        return date.strftime('%a %d %b %Y, %I:%M%p')
