5305a4ba71f327cd0f8d1807914a6f58f70275c1
benjyongzh <benj_yongzh@hotmail.com>
2024-08-16 22:30:24 +0800

    Strong typing

--- common/util/utils.py
+++ common/util/utils.py
@@ -5,12 +5,12 @@ from django.apps import apps
 #         from timestamps.admin import update_tables_last_updated
 #         update_tables_last_updated(table_name, key)
 
-def update_timestamps_table_lastupdated(table_name:str):
+def update_timestamps_table_lastupdated(table_name:str) -> str:
     if apps.is_installed("timestamps"):
         from timestamps.admin import update_table_last_updated
         return update_table_last_updated(table_name)
 
-def get_table_lastupdated_datetime(table_name:str):
+def get_table_lastupdated_datetime(table_name:str) -> str:
     if apps.is_installed("timestamps"):
         from timestamps.admin import get_table_last_updated
         return get_table_last_updated(table_name)
--- timestamps/admin.py
+++ timestamps/admin.py
@@ -4,39 +4,44 @@ from api.serializers import TablesLastUpdatedSerializer
 from datetime import datetime, timedelta
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
+from django.core import serializers
+
 
 # Register your models here.
 @admin.register(TablesLastUpdated)
 class TablesLastUpdatedAdmin(admin.ModelAdmin):
     list_display = ['table', 'last_updated']
 
-def update_table_last_updated(table_name:str):
+def update_table_last_updated(table_name:str) -> str:
     try:
-        last_updated = TablesLastUpdated.objects.update(table=table_name)
-    except ObjectDoesNotExist as e:
-        last_updated = TablesLastUpdated.objects.create(table=table_name)
-    finally:
+        last_updated = TablesLastUpdated.objects.update_or_create(table=table_name)
         date_object = get_datetime_object_from_model_object(last_updated)
         return format_datetime_object_to_string(date_object)
+    except Exception as e:
+        return str(e)
     
-def get_table_last_updated(table_name:str):
+def get_table_last_updated(table_name:str) -> str:
     try:
         last_updated = TablesLastUpdated.objects.get(table=table_name)
     except ObjectDoesNotExist as e:
-        # should let function return None if really None
-        last_updated = TablesLastUpdated.objects.create(table=table_name)
-    finally:
-        date_object = get_datetime_object_from_model_object(last_updated)
-        return format_datetime_object_to_string(date_object)
+        return "Nil"
+    except Exception as e:
+        return str(e)
+    date_object = get_datetime_object_from_model_object(last_updated)
+    return format_datetime_object_to_string(date_object)
 
 def get_datetime_object_from_model_object(obj):
-        serializer = TablesLastUpdatedSerializer(obj)
-        raw_date = serializer.data['last_updated'].split(".")[0]
-        return datetime.strptime(raw_date,'%Y-%m-%dT%H:%M:%S')
+    print(obj)
+    # serializer = serializers.serialize("json", obj)
+    serializer = TablesLastUpdatedSerializer(obj)
+    print(serializer.data)
+    raw_date = serializer.data['last_updated'].split(".")[0]
+    print("raw_date: ", raw_date)
+    return datetime.strptime(raw_date,'%Y-%m-%dT%H:%M:%S')
     
-def format_datetime_object_to_string(datetime_object):
+def format_datetime_object_to_string(datetime_object) -> str:
     # set timezone
-    if settings.TIMEZONE_HOURS:
+    if settings.TIMEZONE_HOURS and settings.TIMEZONE_HOURS != 0:
         date = datetime_object + timedelta(hours=settings.TIMEZONE_HOURS)
         if settings.TIMEZONE_HOURS > 0:
             timezone_str: str = f"GMT +{settings.TIMEZONE_HOURS}:00"
@@ -46,4 +51,5 @@ def format_datetime_object_to_string(datetime_object):
         timezone_str: str = "GMT"
             
     final_str: str = f"{date.strftime('%a %d %b %Y, %I:%M%p')}, {timezone_str}"
+    print("final_str: ", final_str)
     return final_str
