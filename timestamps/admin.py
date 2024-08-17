119b0cf555a3d15764f8e2fdb19d4ad503d08a66
benjyongzh <benj_yongzh@hotmail.com>
2024-08-17 18:47:07 +0800

    Fix: update_or_create returns tuple. not just obj

--- timestamps/admin.py
+++ timestamps/admin.py
@@ -4,8 +4,6 @@ from api.serializers import TablesLastUpdatedSerializer
 from datetime import datetime, timedelta
 from django.core.exceptions import ObjectDoesNotExist
 from django.conf import settings
-from django.core import serializers
-
 
 # Register your models here.
 @admin.register(TablesLastUpdated)
@@ -14,7 +12,7 @@ class TablesLastUpdatedAdmin(admin.ModelAdmin):
 
 def update_table_last_updated(table_name:str) -> str:
     try:
-        last_updated = TablesLastUpdated.objects.update_or_create(table=table_name)
+        last_updated = TablesLastUpdated.objects.update_or_create(table=table_name)[0]
         date_object = get_datetime_object_from_model_object(last_updated)
         return format_datetime_object_to_string(date_object)
     except Exception as e:
@@ -31,12 +29,8 @@ def get_table_last_updated(table_name:str) -> str:
     return format_datetime_object_to_string(date_object)
 
 def get_datetime_object_from_model_object(obj):
-    print(obj)
-    # serializer = serializers.serialize("json", obj)
     serializer = TablesLastUpdatedSerializer(obj)
-    print(serializer.data)
     raw_date = serializer.data['last_updated'].split(".")[0]
-    print("raw_date: ", raw_date)
     return datetime.strptime(raw_date,'%Y-%m-%dT%H:%M:%S')
     
 def format_datetime_object_to_string(datetime_object) -> str:
@@ -49,7 +43,5 @@ def format_datetime_object_to_string(datetime_object) -> str:
             timezone_str: str = f"GMT -{abs(settings.TIMEZONE_HOURS)}:00"
     else:
         timezone_str: str = "GMT"
-            
     final_str: str = f"{date.strftime('%a %d %b %Y, %I:%M%p')}, {timezone_str}"
-    print("final_str: ", final_str)
     return final_str
