7861acd8a68bc997c09a5694cafa835a52fd120d
benjyongzh <benj_yongzh@hotmail.com>
2024-08-18 15:34:00 +0800

    Pagination for API using ListAPIView

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
      path('resale-transactions/', views.get_resale_prices),
      path('postal-codes/', views.get_postal_codes),
      path('building-polygons/', views.get_building_polygons),
+     path('resale-transactions/latest', views.latest_price_overview.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.generics import ListAPIView
 from resaletransactions.models import ResaleTransaction
 from postalcodes.models import PostalCodeAddress, BuildingGeometryPolygon
 from .serializers import ResaleTransactionSerializer, PostalCodeAddressSerializer, BuildingGeometryPolygonSerializer
@@ -43,3 +45,33 @@ def get_building_polygons(request):
     polygons = BuildingGeometryPolygon.objects.all()
     serializer = BuildingGeometryPolygonSerializer(polygons, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+# return the average price of a block within a past given timeframe
+class average_price_overview(APIView):
+    def get(self, request):
+        timeframe:str = request.query_params['timeframe']
+        pass
+
+# return the latest price of a block
+class latest_price_overview(ListAPIView):
+    # select latest transaction per block
+    queryset = ResaleTransaction.objects.order_by(
+            "town",
+            "flat_type",
+            "block",
+            "street_name",
+            "floor_area_sqm",
+            "flat_model",
+            "storey_range",
+            "-id").distinct(
+                "town",
+                "flat_type",
+                "block",
+                "street_name",
+                "floor_area_sqm",
+                "flat_model",
+                "storey_range",)
+    
+    # join latest_transaction with postalcodeaddress ON same block and street_name
+    # join 2nd table with polygons ON same postalcode
+    serializer_class = ResaleTransactionSerializer
