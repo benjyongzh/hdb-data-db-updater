e85b544076dcf0b318d23db53e5a63384c62588b
benjyongzh <benj_yongzh@hotmail.com>
2024-08-20 22:38:23 +0800

    Create function for setting postal code IDs

--- resaletransactions/util/csv_operations.py
+++ resaletransactions/util/csv_operations.py
@@ -3,13 +3,18 @@ import psycopg2
 from common.util.get_latest_file_in_folder import get_latest_file_in_folder
 from config.env import env
 import pandas as pd
-from sqlalchemy import create_engine
-from sqlalchemy import types
+from sqlalchemy import create_engine,types
+from postalcodes.util.postal_codes import get_postal_code_from_address
+from postalcodes.models import PostalCodeAddress
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def update_resaletransactions_table_with_csv(table_name,csv_file) -> None:
     # pandas dataframe of file
     dataframe = pd.read_csv(csv_file)
     dataframe['postal_code_id_id'] = None
+
+    set_postal_code_ids(dataframe)
 
     # get db connection
     db_connection_url = f"postgresql://{env('DB_USER')}:{env('DB_PASSWORD')}@{env('DB_HOST')}:{env('DB_PORT')}/{env('DB_NAME')}"
@@ -32,6 +37,40 @@ def update_resaletransactions_table_with_csv(table_name,csv_file) -> None:
                         'postal_code_id_id': types.BigInteger(),
                         })
 
+    # set_primary_key(table_name, "id")
+    
+def set_primary_key(table_name:str, pk_column_name: str):
+    pass
+
+def set_postal_code_ids(dataframe):
+    for i in range(len(dataframe.index)):
+        block:str = dataframe.at[i, 'block']
+        street_name:str = dataframe.at[i, 'street_name']
+        try:
+            postalcode_object = PostalCodeAddress.objects.get(block=block, street_name=street_name)
+            postalcode:str = postalcode_object['postal_code']
+            dataframe.at[i, 'postal_code_id_id'] = postalcode
+        except ObjectDoesNotExist as e:
+            try:
+                address:str = f"{block} {street_name}"
+                postalcode:str = get_postal_code_from_address(address)
+                dataframe.at[i, 'postal_code_id_id'] = postalcode
+            except Exception as e:
+                print(f"""
+                        Error for '{address}' in being registered as new address:
+                        {e}
+                    """)
+                continue
+        except Exception as e:
+            print(f"""
+                    Error for reaching postal code table:
+                    {e}
+                """)
+            continue
+        
+
+        
+        
 
 def import_from_csv_to_db(table_name, folderpath):
     conn = psycopg2.connect(host=env("DB_HOST"),dbname = env("DB_NAME"), user=env("DB_USER"), password=env("DB_PASSWORD"), port=env("DB_PORT"))
