5464c2db6b939238852e2816c9a0041b8ff9ad12
benjyongzh <benj_yongzh@hotmail.com>
2024-08-24 23:09:31 +0800

    Add params to URLs for API

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('resale-transactions/', views.get_all_resale_prices.as_view()),
-     path('postal-codes/', views.get_all_postal_codes.as_view()),
-     path('building-polygons/', views.get_all_building_polygons.as_view()),
-     path('resale-transactions/latest', views.latest_price_overview.as_view()),
+     path('resale-transactions/', views.get_all_resale_prices.as_view(), name='list-resaletransactions'),
+     path('postal-codes/', views.get_all_postal_codes.as_view(), name='list-postalcodes'),
+     path('building-polygons/', views.get_all_building_polygons.as_view(), name='list-polygons'),
+     path('resale-transactions/average/<int:timeframe>/', views.average_price_overview.as_view(), name='list-resaletransactions-average'),
+     path('resale-transactions/latest/', views.latest_price_overview.as_view(), name='list-resaletransactions-latest'),
 ]
