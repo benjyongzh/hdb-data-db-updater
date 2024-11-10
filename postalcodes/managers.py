08f238b8c1476831c495c9e259e54b325388d508
benjyongzh <benj_yongzh@hotmail.com>
2024-11-10 22:28:13 +0800

    try raw sql. altho not recommended. still having issues

--- postalcodes/managers.py
+++ postalcodes/managers.py
@@ -1,8 +1,9 @@
-from django.db.models import Subquery, OuterRef, DecimalField, Value, Avg, F
+from django.db.models import Subquery, OuterRef, DecimalField, Avg, Value, Case, When
 from django.db.models.functions import RowNumber,Coalesce
 from django.db.models.expressions import Window
 from django.contrib.gis.db import models
 from django.apps import apps  # Required for lazy import
+from django.db import connection
 
 class PostalCodeAddressQuerySet(models.QuerySet):
 
@@ -19,36 +20,84 @@ class PostalCodeAddressQuerySet(models.QuerySet):
         ResaleTransaction = apps.get_model('resaletransactions', 'ResaleTransaction')  # Lazy import of FlatPrice from other app  
         # TODO fix latest_price always being same number
             
-        queryset = ResaleTransaction.objects \
-            .distinct(
-                'postal_code_key',
-                "town",
-                "flat_type",
-                "block",
-                "street_name",
-                "floor_area_sqm",
-                "storey_range",) \
-            .order_by(
-                'postal_code_key',
-                "town",
-                "flat_type",
-                "block",
-                "street_name",
-                "floor_area_sqm",
-                "storey_range",
-                "-id")
+        # queryset = ResaleTransaction.objects \
+        #     .distinct(
+        #         'postal_code_key',
+        #         "town",
+        #         "flat_type",
+        #         "block",
+        #         "street_name",
+        #         "floor_area_sqm",
+        #         "storey_range",) \
+        #     .order_by(
+        #         'postal_code_key',
+        #         "town",
+        #         "flat_type",
+        #         "block",
+        #         "street_name",
+        #         "floor_area_sqm",
+        #         "storey_range",
+        #         "-id")
+        # latest_prices = ResaleTransaction.objects \
+        #     .filter(id__in=Subquery(queryset.only("id"))) \
+        #     .values('postal_code_key') \
+        #     .annotate(average_latest_price=Avg('resale_price')) \
+        #     .values('postal_code_key', 'average_latest_price')
         
-        latest_prices = ResaleTransaction.objects \
-            .filter(id__in=Subquery(queryset.only("id"))) \
-            .values('postal_code_key') \
-            .annotate(average_latest_price=Avg('resale_price'))
-            # .values('average_latest_price')
+        # latest_prices = ResaleTransaction.objects \
+        #     .values('postal_code_key__postal_code') \
+        #     .annotate(average_latest_price=Avg('resale_price')) \
+        #     .values('postal_code_key__postal_code', 'average_latest_price')
 
-        print(latest_prices)
-        
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                WITH latest_transactions AS (
+                    SELECT DISTINCT ON (postal_code_key_id, flat_type, storey_range, floor_area_sqm)
+                        postal_code_key_id,
+                        town,
+                        block,
+                        street_name,
+                        flat_type,
+                        storey_range,
+                        floor_area_sqm,
+                        resale_price
+                    FROM
+                        resaletransactions_resaletransaction
+                    ORDER BY 
+                        postal_code_key_id, flat_type, storey_range, floor_area_sqm, id DESC
+                )
+                SELECT
+                    postal_code_key_id,
+                    AVG(resale_price) AS average_price
+                FROM
+                    latest_transactions
+                GROUP BY
+                    postal_code_key_id;
+            """)
+            results = cursor.fetchall()
+
+        # Step 2: Map the SQL results to a dictionary for fast lookup
+        average_prices = {
+            row[0]: row[1]  # (postal_code_key_id) -> average_price
+            for row in results
+        }
+
+        # Step 3: Use Case/When expressions to annotate `self` with the average price
+        annotations = [
+            When(
+                postal_code=postal_code_key_id,
+                then=Value(average_price, output_field=DecimalField(max_digits=12, decimal_places=2))
+            )
+            for postal_code_key_id, average_price in average_prices.items()
+        ]
+
         return self.annotate(
-            # latest_price=Subquery(latest_prices.filter(postal_code_key=OuterRef('pk')).values('average_latest_price')[:1])
-            latest_price=Subquery(ResaleTransaction.objects.filter(postal_code_key=Subquery(latest_prices).postal_code_key).values('average_latest_price')[:1])
+            latest_price=Case(*annotations, default=Value(0, output_field=DecimalField(max_digits=12, decimal_places=2)))
+            # ! always getting default only
+            # latest_price=Subquery(
+            #     latest_prices.filter(postal_code_key__postal_code=OuterRef('postal_code_key__postal_code'))
+            #     .values('average_latest_price')[:1]
+            # )
             # latest_price=Coalesce(
             #     Subquery(latest_price, output_field=DecimalField(max_digits=12, decimal_places=2)),
             #     Value(0,output_field=DecimalField(max_digits=12, decimal_places=2))
