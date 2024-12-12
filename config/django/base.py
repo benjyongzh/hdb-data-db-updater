57aba7bf34674c5413acdf51e5367a7d64a17986
benjyongzh <benj_yongzh@hotmail.com>
2024-12-12 19:28:08 +0800

    Add zipping middleware

--- config/django/base.py
+++ config/django/base.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
